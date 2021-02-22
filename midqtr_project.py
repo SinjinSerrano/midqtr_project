dfa6a46c3b3256353059a50eeb9acf96523dbe8e
SinjinSerrano <sinjin.serrano@gmail.com>
2021-02-22 15:06:55 -0800

    Part 1 Draft

--- midqtr_project.py
+++ midqtr_project.py
@@ -1,51 +1,99 @@
 """
 DSC 20 Mid-Quarter Project
-Name: TODO
-PID:  TODO
+Name: Sinjin Serrano and David Perez
+PID:  A16687329 / A________
 """
 
 # Part 1: RGB Image #
 class RGBImage:
     """
-    TODO: add description
+    TODO: review description
+    Creates a RGBImage class, an abstraction of an image using RGB colorspace.
+
+    The RGB color model is a tuple of three integers (intensity) indicating
+    the presence of color in a given pixel (Red, Green, Blue).
+    Values are unsigned 8-bit integers, meaning they range from 0,
+    indicating no presence, and 255, indicating full presence.
+
+    'pixels' is a three-dimensional list used to store RGBImage's data.
+    The first dimension is an integer indicating which color channel is
+    represented (0 - red, 1 - green, 2 - blue)
+    The second dimension indicates the pixel's row index, while the third
+    dimension indicates the pixel's column index.
+    The value stored there is a three-element tuple representing using the
+    RGB color model to store that pixel's value.
+
+    TODO: Test methods.
     """
 
     def __init__(self, pixels):
         """
-        TODO: add description
+        A constructor for RGBImage that defines the image's pixel list.
+        Assume:
+            - Index of channel c is guaranteed to be 0, 1, or 2
+            - Each channel contains a valid (row x col) matrix
         """
-        # YOUR CODE GOES HERE #
-        self.pixels = ...  # initialze the pixels list here
+        self.pixels = pixels
 
     def size(self):
         """
-        TODO: add description
+        A getter method that calculates the size of the image and returns
+        a tuple containing (rows, columns).
         """
-        # YOUR CODE GOES HERE #
+        rows = len(self.pixels[0])
+        cols = len(self.pixels[0][0])
+        return (rows, cols)
 
     def get_pixels(self):
         """
-        TODO: add description
+        A getter method that returns a deep copy of the pixels matrix.
         """
-        # YOUR CODE GOES HERE #
+        return [[[intensity for intensity in row] for row in channel]\
+             for channel in self.pixels]
 
     def copy(self):
         """
-        TODO: add description
+        A method which returns a copy of this instance, using a deep copy of
+        the pixels matrix.
         """
-        # YOUR CODE GOES HERE #
+        return RGBImage(self.get_pixels())
 
     def get_pixel(self, row, col):
         """
-        TODO: add description
+        A getter method which returns a three-element tuple containing the
+        color of the pixel at the specified row and col.
         """
-        # YOUR CODE GOES HERE #
+        assert isinstance(row, int) and isinstance(col, int)
+        assert 0 <= row < self.size()[0] and 0 <= col < self.size()[1]
+        iRED = 0
+        iGREEN = 1
+        iBLUE = 2
+        return (self.pixels[iRED][row][col], self.pixels[iGREEN][row][col], self.pixels[iBLUE][row][col])
 
     def set_pixel(self, row, col, new_color):
         """
-        TODO: add description
+        A setter method which updates the pixel at the specified row and col
+        with a new color, represented by a three-element tuple.
+        If an intensity is represented by -1, do not update the intensity
+        at that channel.
         """
-        # YOUR CODE GOES HERE #
+        assert isinstance(row, int) and isinstance(col, int)
+        assert 0 <= row < self.size()[0] and 0 <= col < self.size()[1]
+        assert all([isinstance(i, int) for i in new_color])
+        assert all([-1 <= i <= 255 for i in new_color])
+        iRED = 0
+        iGREEN = 1
+        iBLUE = 2
+        color = self.get_pixel(row, col)
+        if new_color[iRED] != -1:
+            color[iRED] = new_color[iRED]
+        if new_color[iGREEN] != -1:
+            color[iGREEN] = new_color[iGREEN]
+        if new_color[iBLUE] != -1:
+            color[iBLUE] = new_color[iBLUE]
+        for i in range(3):
+            self.pixel[i][row][col] = color[i]
+
 
 
 # Part 2: Image Processing Methods #
